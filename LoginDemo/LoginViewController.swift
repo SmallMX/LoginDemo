46004fd2b78c3d655b3751c65a3543169881df1f
MingXin Liu <mx@MingXindeMac-mini.local>
2023-07-08 13:12:06 +0800

    登陆UI

---
+++ LoginDemo/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  LoginDemo
+//
+//  Created by MingXin Liu on 2023/7/8.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var accountRemindLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordRemindLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
